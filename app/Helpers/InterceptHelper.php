0b6d829eca7227d407d89eacf61c68e0009cbc1d
JohanCruz <johandanielcruz@gmail.com>
2024-11-16 13:51:33 -0500

    fix interception

--- app/Helpers/InterceptHelper.php
+++ app/Helpers/InterceptHelper.php
@@ -1,21 +1,25 @@
-<?php
-
 use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Request;
 
-if (!function_exists('intercept')) {
-    function intercept($controller, $method, ...$params)
-    {
-        try {
-            return call_user_func_array([$controller, $method], $params);
-        } catch (\Throwable $e) {
-            Log::error('Error in ' . $method, [
-                'message' => $e->getMessage(),
-                'trace' => $e->getTraceAsString(),
-            ]);
+function intercept($controller, $method, ...$params)
+{
+    try {
+        $reflection = new ReflectionMethod($controller, $method);
+        $parameters = $reflection->getParameters();
 
-            return response()->json([
-                'error' => 'An error occurred. Please try again later.',
-            ], 500);
+        if (isset($parameters[0]) && $parameters[0]->getType()->getName() === Request::class) {
+            array_unshift($params, app(Request::class));
         }
+
+        return call_user_func_array([$controller, $method], $params);
+    } catch (\Throwable $e) {
+        Log::error('Error in ' . $method, [
+            'message' => $e->getMessage(),
+            'trace' => $e->getTraceAsString(),
+        ]);
+
+        return response()->json([
+            'error' => 'An error occurred. Please try again later.',
+        ], 500);
     }
 }
